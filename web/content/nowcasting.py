4017b033e2f2e2e2c4d5c1b48dc7295be30af743
Pamela Anne Serrano <pam@whitewidget.com>
2025-10-22 23:37:32 +0800

    fix(radar): remove running of prediction script

--- web/content/nowcasting.py
+++ web/content/nowcasting.py
@@ -1,12 +1,9 @@
 import streamlit as st
-from backend.predict import predict_main
 from backend.radar_data import generate_radar_data
 
 def render_nowcasting():
     st.markdown("### 📊 RAINLOOP Nowcasting Data")
-    predict = predict_main()
-    if predict:
-        processed_data = generate_radar_data()
+    processed_data = generate_radar_data()
 
     if processed_data:
         st.success("✅ RAINLOOP backend data loaded successfully!")
